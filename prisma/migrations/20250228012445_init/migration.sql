d0c5a342cdfe3ddbf34c75a0ca84e1afb98bd31f
Roque-55 <edurss1000@gmail.com>
2025-03-25 03:20:48 -0300

    gitgnore update

--- prisma/migrations/20250228012445_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isInactive` on the `ReasonCategory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ReasonCategory" DROP COLUMN "isInactive",
-ADD COLUMN     "isActivation" BOOLEAN NOT NULL DEFAULT false;
